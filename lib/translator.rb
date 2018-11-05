1520a58bbbaac4476680034d5630dd8a67250e64
Daniel Hamilton <danhamilton16@gmail.com>
2018-11-05 23:02:39 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,8 @@
 require "yaml"
 def load_library(path)
   library=YAML.load_file(path)
+  return_hash={"get_meaning"=>{}, "get_emoticon"=>{}}
+  return return_hash
 end
 
 def get_japanese_emoticon
@@ -11,3 +13,5 @@ end
 def get_english_meaning
   # code goes here
 end
+
+load_library()
